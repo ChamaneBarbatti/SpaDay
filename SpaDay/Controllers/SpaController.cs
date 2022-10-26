74e8cb99cbfb632f4c0779b52a5492b4123b0274
Chamane Barbatti <chamane@sbcglobal.net>
2022-10-26 14:32:33 -0500

    Finished Studio

--- SpaDay/Controllers/SpaController.cs
+++ SpaDay/Controllers/SpaController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-// For more information on enabling MVC    for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace SpaDay.Controllers
 {
@@ -55,6 +55,11 @@ namespace SpaDay.Controllers
                     appropriateFacials.Add(facials[i]);
                 }
             }
+            ViewBag.skintype= skintype;
+            ViewBag.appropriateFacials = appropriateFacials;
+            ViewBag.name = name;
+            ViewBag.manipedi = manipedi;
+     
             return View();
         }
 
